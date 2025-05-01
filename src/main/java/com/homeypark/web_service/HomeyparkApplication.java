120154c63b9565ace7d9485c6c6c872e1f1fa540
Dark7YT <u202210846@upc.edu.pe>
2025-04-30 22:00:03 -0500

    first commit

---
+++ src/main/java/com/homeypark/web_service/HomeyparkApplication.java
@@ -0,0 +1,13 @@
+package com.homeypark.web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeyparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeyparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homeypark/web_service/HomeyparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeypark.web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeyparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
