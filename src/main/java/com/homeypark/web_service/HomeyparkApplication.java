144db621691493bb4f6463033d2a6ad12f4d494c
AdrianoSCruzP <u202210697@upc.edu.pe>
2025-06-16 19:52:17 -0500

    feat(config): load variables from .env.production using dotenv-java

--- src/main/java/com/homeypark/web_service/HomeyparkApplication.java
+++ src/main/java/com/homeypark/web_service/HomeyparkApplication.java
@@ -1,5 +1,6 @@
 package com.homeypark.web_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,7 +10,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class HomeyparkApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.filename(".env.production")
+				.load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(HomeyparkApplication.class, args);
 	}
-
 }
