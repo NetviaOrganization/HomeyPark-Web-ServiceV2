c24f96ea20d65b4aecc9cf140c97d462eea6b3b4
AdrianoSCruzP <u202210697@upc.edu.pe>
2025-06-16 20:51:44 -0500

    fix: remove dotenv configuration to resolve deployment error

--- src/main/java/com/homeypark/web_service/HomeyparkApplication.java
+++ src/main/java/com/homeypark/web_service/HomeyparkApplication.java
@@ -1,6 +1,5 @@
 package com.homeypark.web_service;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,14 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class HomeyparkApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure()
-				.filename(".env.production")
-				.load();
-
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USER", dotenv.get("DB_USER"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
 		SpringApplication.run(HomeyparkApplication.class, args);
 	}
 }
